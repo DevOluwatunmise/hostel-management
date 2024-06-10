449aceb3c144c1bc34be4d1be27ef9f3a9f7c00f
DevOluwatunmise <tunmisedare@gmail.com>
2024-06-10 11:17:34 +0100

    update

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Student = require("../models/studentModel");
 const Room = require("../models/roomModel");
 const { generateUniqueId } = require("../utils/generateUniqueId");
+const { checkout } = require("../routes/roomRoute");
 
 // To generate new uniqueId for new student which is must be different from already generated one
 // if the random uniqueId generated is the same, it should re start again to generate new uniqueIdar
@@ -22,27 +23,28 @@ const ensureUniqueId = async () => {
 const date = new Date();
 
 const formatDate = (input) => {
+  // if the input(time) is less than 9 it shoul add 0 to the front (01 - 09) bcos we are using 24hrs not 12
   return input > 9 ? input : `0${input}`;
-}
+};
 
 const formatHour = (input) => {
-  return input > 12 ? input - 12 : input
-}
+  return input > 12 ? input - 12 : input;
+};
 
 const format = {
   dd: formatDate(date.getDate()),
-  mm: formatDate(date.getMonth()),
+  mm: formatDate(date.getMonth() + 1), // we add +1 bcos javascript start from 0
   yyyy: formatDate(date.getFullYear()),
 
   HH: formatDate(date.getHours()),
   hh: formatDate(formatHour(date.getHours())),
   MM: formatDate(date.getMinutes()),
   SS: formatDate(date.getSeconds()),
-}
+};
 
-const format24Hour = ({dd, mm, yyyy, HH, MM, SS}) => {
-  return `${mm}/${dd}/{yyyy} ${HH}:${MM}:${SS}`
-}
+const format24Hour = ({ dd, mm, yyyy, HH, MM, SS }) => {
+  return `${mm}/${dd}/${yyyy} ${HH}:${MM}:${SS}`;
+};
 
 const registerStudent = asyncHandler(async (req, res) => {
   try {
@@ -157,118 +159,126 @@ const updateStudentProfile = asyncHandler(async (req, res) => {
 });
 
 const changeStudentRoom = asyncHandler(async (req, res) => {
-  const {studentId, newRoomNum} = req.body
+  const { studentId, newRoomNum } = req.body;
 
-  const student = await Student.findById(studentId)
+  const student = await Student.findById(studentId);
 
-  if(!student) {
-    return res.status(404).json({message: "Student noy found"})
+  if (!student) {
+    return res.status(404).json({ message: "Student noy found" });
   }
 
-  const currentRoom = await Room.findById(student.room)
+  const currentRoom = await Room.findById(student.room);
 
-  if(currentRoom) {
+  if (currentRoom) {
     currentRoom.roomOccupancy = currentRoom.roomOccupancy.filter(
       (occupant) => occupant.toString() !== studentId
-    )
-    
-    if(currentRoom.roomOccupancy.length < currentRoom.roomCapacity) {
-      currentRoom.status = "available"
+    );
+
+    if (currentRoom.roomOccupancy.length < currentRoom.roomCapacity) {
+      currentRoom.status = "available";
     }
 
-    await currentRoom.save()
+    await currentRoom.save();
   }
 
-  const newRoom = await Room.findOne({roomNumber: newRoomNum});
+  const newRoom = await Room.findOne({ roomNumber: newRoomNum });
 
   if (!newRoom) {
-    return res.status(404).json({message: "New room not found"})
+    return res.status(404).json({ message: "New room not found" });
   }
-  if (newRoom.roomStatus !== "available" ){
-    return res.status(400).json({message: "New room is not available"})
+  if (newRoom.roomStatus !== "available") {
+    return res.status(400).json({ message: "New room is not available" });
   }
 
-  student.room = newRoom._id
+  student.room = newRoom._id;
 
-  newRoom.roomOccupancy.push(student._id)
+  newRoom.roomOccupancy.push(student._id);
 
   if (newRoom.roomOccupancy.length >= newRoom.roomOccupancy) {
-    newRoom.roomStatus = "unavailable"
+    newRoom.roomStatus = "unavailable";
   }
 
-  await newRoom.save()
-  await student.save()
+  await newRoom.save();
+  await student.save();
 
-  res.status(200).json({message: "Room change succesfully", student, newRoom})
-
+  res
+    .status(200)
+    .json({ message: "Room change succesfully", student, newRoom });
 });
 
 const updateCheckedInStatus = asyncHandler(async (req, res) => {
-  const {studentId, action} = req.body
+  const { studentId, action, roomNumber } = req.body;
 
-  const student = await Student.findById(studentId)
+  const student = await Student.findById(studentId);
 
   if (!student) {
-    return res.status(404).json({messge: "Student not found"})
+    return res.status(404).json({ messge: "Student not found" });
   }
 
-  if (action === "checkIn"){
+  if (action === "checkIn") {
     student.checkedIn = true;
     student.checkedInTime = format24Hour(format);
-  } else if(action === "checkOut") {
+  } else if (action === "checkOut") {
     student.checkedIn = false;
     student.checkedOutTime = format24Hour(format);
-  }else {
+  } else {
     return res.status(400).json({
-      message: "Invalid action"
-  })
+      message: "Invalid action",
+    });
   }
 
-  await Room.updateMany(
-    {roomOccupancy: studentId},
-    {$pull: {roomOccupancy: studentId}}
-  )
+  const room = await Room.findOne({ roomNumber });
+  if (!room) {
+    return res.status(404).json({ msg: "Room not found" });
+  }
 
+  if (action === "checkIn") {
+    room.roomOccupancy.push(studentId);
+  } else if (action === "checkout") {
+    room.roomOccupancy.pull(studentId);
+  }
+
+  await room.save();
   await student.save();
 
-  res.status(200).json({message: `Student ${action} successfully`, student})
- 
-  }
-);
+  res
+    .status(200)
+    .json({ message: `Student ${action} successfully`, student, room });
+});
 
 const deleteStudent = asyncHandler(async (req, res) => {
-  const studentId  = req.params._id;
+  const studentId = req.params._id;
 
-  try{
-   const student = await Student.findById(studentId);
+  try {
+    const student = await Student.findById(studentId);
     if (!student) {
-    res.status(404);
-    throw new Error("Student not found in database");
-   }
-
-   const room = await Room.findById(student.room)
-
-   if (room) {
-    room.roomOccupancy = room.roomOccupancy.filter(
-      (occupant) => occupant.toString() !== studentId
-    )
-
-    if (room.roomOccupancy.length < room.roomCapacity) {
-      room.roomStatus = "available"
+      res.status(404);
+      throw new Error("Student not found in database");
     }
 
-    await room.save()
+    const room = await Room.findById(student.room);
+
+    if (room) {
+      room.roomOccupancy = room.roomOccupancy.filter(
+        (occupant) => occupant.toString() !== studentId
+      );
+
+      if (room.roomOccupancy.length < room.roomCapacity) {
+        room.roomStatus = "available";
+      }
+
+      await room.save();
+    }
+    await student.deleteOne();
+    res.status(200).json({
+      message: "Student deleted successfully!",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      message: "Internal server error",
+    });
   }
-  await student.deleteOne();
-  res.status(200).json({
-    message: "Student deleted successfully!",
-  });
-} catch (error) {
-  console.error(error.message)
-  res.status(500).json({
-    message: "Internal server error"
-  })
-}
 });
 
 module.exports = {
--- frontend/src/Component/Dashboard/StudentDashboard.jsx
+++ frontend/src/Component/Dashboard/StudentDashboard.jsx
@@ -1,19 +1,17 @@
-import react, {useEffect, useState} from "react";
+import react, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import "./StudentDashboard.css";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import { IoMenu, IoCloseSharp  } from "react-icons/io5";
-import {FaPenFancy} from "react-icons/fa"
-import {confirmAlert} from "react-confirm-alert";
-import 'react-confirm-alert/src/react-confirm-alert.css'; 
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
+import { FaPenFancy } from "react-icons/fa";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 import useAuthRedirect from "../../../context/useAuth";
 import axios from "axios";
 import UpdateCheckIn from "../Modal/UpdateCheckIn";
 import ChangeStudentRoom from "../Modal/ChangeStudentRoom";
 import UpdateStudentProfile from "../Modal/UpdateStudentProfile";
-
-
 
 const StudentDashboard = () => {
   const [search, setSearch] = useState("");
@@ -25,41 +23,40 @@ const StudentDashboard = () => {
   const [selectedModal, setSelectedModal] = useState("");
   const [selectedStudent, setSelectedStudent] = useState(null);
 
-
   useEffect(() => {
-    const fetchStudents = async() => {
+    const fetchStudents = async () => {
       try {
         const response = await axios.get("http://localhost:3500/student/");
         setData(response.data);
       } catch (error) {
-        console.erroe("Error fetching data:", error)
+        console.erroe("Error fetching data:", error);
       }
-    }
+    };
 
     fetchStudents();
-  },[])
+  }, []);
 
   const handleModalOpen = (student) => {
     setSelectedStudent(student);
     setIsModalOpen(true);
-  }
+  };
 
   const handleModalClose = () => {
-    setSelectedModal();
+    setSelectedModal("");
     setIsModalOpen(false);
     setSelectedStudent(null);
-  }
+  };
 
-  const handleModalSelect = (modalType) => {
-    setSelectedModal(modalType)
-  }
+  const handleModalSelected = (modalType) => {
+    setSelectedModal(modalType);
+  };
 
   const removeUser = async (_id) => {
     try {
       console.log(`Delete student by id: ${_id}`);
       const response = await axios.delete(
         `http://localhost:3500/student/delete-student/${_id}`
-      )
+      );
 
       // console.log(response.data)
 
@@ -68,15 +65,12 @@ const StudentDashboard = () => {
 
       // setting success message
       setMessage("Student deleted successdully");
-
-
-      
     } catch (error) {
-       //seting error message
-      setMessage("Failed to delete student")
-      console.error("Error deleting:", error)
+      //seting error message
+      setMessage("Failed to delete student");
+      console.error("Error deleting:", error);
     }
-  }
+  };
 
   // to confirm that, are yu truely want to delete the data or not(double check) when it pulp up
   const confirmDelete = (_id) => {
@@ -86,178 +80,156 @@ const StudentDashboard = () => {
       buttons: [
         {
           label: "Delete",
-          onClick: () => removeUser(_id) 
+          onClick: () => removeUser(_id),
         },
         {
           label: "Cancel",
           onClick: () => alert("Deletion cancelled"),
-        }
-      ]
-    })
-  }
+        },
+      ],
+    });
+  };
 
   const filteredData = data.filter(
-    (item) => 
-      item.nationality.toLowerCase().includes(search.toLowerCase()) || 
+    (item) =>
+      item.nationality.toLowerCase().includes(search.toLowerCase()) ||
       item.email.toLowerCase().includes(search.toLowerCase())
-
-  )
+  );
 
   //
 
   return (
-
     <div>
       {sidebarToggle && (
         <div className="mobile-side-nav">
-        <Sidebar />
+          <Sidebar />
         </div>
       )}
 
       <div className="--flex --overflow-hidden">
-      <div className="desktop-side-nav">
-      <Sidebar />
+        <div className="desktop-side-nav">
+          <Sidebar />
+        </div>
+
+        <div className="--flex-dir-column --overflow-y-auto --flex-1 --overflow-x-hidden ">
+          <main className="--flex-justify-center w-full">
+            <div className="right dash-main">
+              <div className="--flex-justify-between">
+                <p>Students</p>
+                {sidebarToggle ? (
+                  <IoCloseSharp
+                    className="sidebar-toggle-iconB"
+                    onClick={() => setSidebarToggle(false)}
+                  />
+                ) : (
+                  <IoMenu
+                    className="sidebar-toggle-iconB"
+                    onClick={() => setSidebarToggle(true)}
+                  />
+                )}
+              </div>
+              <p>Search students</p>
+              <input
+                placeholder="Search by name, email, or ID number"
+                type="text"
+                className="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+
+              <div className="table">
+                <table className="table_wrapper">
+                  <thead className="table__head">
+                    <tr className="table__row">
+                      <th className="same_class">Student Name</th>
+                      <th className="same_class">Email</th>
+                      <th className="same_class">ID Number</th>
+                      <th className="same_class">Gender</th>
+                      <th className="same_class">Age</th>
+                      <th className="same_class">Nationality</th>
+                      <th className="same_class">Actions</th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="table__body">
+                    {filteredData.map((student, index) => (
+                      <tr key={index} className="table__row">
+                        <td className="same_class">{student.name}</td>
+                        <td className="same_class">{student.email}</td>
+                        <td className="same_class">{student._id}</td>
+                        <td className="same_class">{student.gender}</td>
+                        <td className="same_class">{student.age}</td>
+                        <td className="same_class">{student.nationality}</td>
+                        <td className="same_class">
+                          <RiDeleteBin6Line
+                            size={25}
+                            color="red"
+                            onClick={() => confirmDelete(student._id)}
+                          />
+                          &nbsp;&nbsp;
+                          <FaPenFancy
+                            size={25}
+                            color="blue"
+                            onClick={() => handleModalOpen(student)}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <button className="btn-secondary">
+                <Link to="/student-reg"> Add a student</Link>
+              </button>
+            </div>
+          </main>
+        </div>
       </div>
 
-      <div className="--flex-dir-column --overflow-y-auto --flex-1 --overflow-x-hidden ">
-     
-     <main className="--flex-justify-center w-full">
-     <div className="right dash-main">
-       <div className="--flex-justify-between">
-       <p>Students</p>
-       {sidebarToggle ? (
+      {isMoadalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Select an Option</h2>
+            <button
+              onClick={() => handleModalSelected("UpdateStudentProfile")}
+              className="one"
+            >
+              Update Student Profile
+            </button>
+            <button
+              onClick={() => handleModalSelected("ChangeStudentRoom")}
+              className="two"
+            >
+              Change Student Room
+            </button>
+            <button
+              onClick={() => handleModalSelected("UpdateCheckIn")}
+              className="three"
+            >
+              Update Check-In
+            </button>
+            <button onClick={handleModalClose}>Close</button>
+          </div>
+        </div>
+      )}
 
-         <IoCloseSharp className="sidebar-toggle-iconB"
-         onClick={() => setSidebarToggle(false)}
-         />
-       ):(
-
-
-       <IoMenu className="sidebar-toggle-iconB"
-       onClick={() => setSidebarToggle(true)}
-
-       />
-       )}
-       </div>
-       <p>Search students</p>
-       <input
-         placeholder="Search by name, email, or ID number"
-         type="text"
-         className="search"
-         value={search}
-         onChange={(e) => setSearch(e.target.value)}
-       />
-
-       <div className="table">
-         <table className="table_wrapper">
-           <thead className="table__head">
-             <tr className="table__row">
-               <th className="same_class">Student Name</th>
-               <th className="same_class">Email</th>
-               <th className="same_class">ID Number</th>
-               <th className="same_class">Gender</th>
-               <th className="same_class">Age</th>
-               <th className="same_class">Nationality</th>
-               <th className="same_class">Actions</th>
-             </tr>
-           </thead>
-
-           <tbody className="table__body">
-             {filteredData.map((student, index) => (
-               <tr key={index} className="table__row">
-                 <td className="same_class">{student.name}</td>
-                 <td className="same_class">{student.email}</td>
-                 <td className="same_class">{student._id}</td>
-                 <td className="same_class">{student.gender}</td>
-                 <td className="same_class">{student.age}</td>
-                 <td className="same_class">{student.nationality}</td>
-                 <td className="same_class">
-                   <RiDeleteBin6Line
-                     size={25}
-                     color="red"
-                     onClick={() => confirmDelete(student._id)}
-                   />
-                   &nbsp;&nbsp;
-                   <FaPenFancy
-                     size={25}
-                     color="blue"
-                     onClick={() => handleModalOpen(student)}
-                     />
-                 </td>
-               </tr>
-             ))}
-           </tbody>
-         </table>
-       </div>
-       <button className="btn-secondary">
-           
-           <Link to='/student-reg'> Add a student</Link>
-       </button>
-     </div>
-     </main>
-     
-   </div>
+      {selectedModal === "UpdateStudentProfile" && (
+        <UpdateStudentProfile
+          student={selectedStudent}
+          onClose={handleModalClose}
+        />
+      )}
+      {selectedModal === "changeStudentRoom" && (
+        <ChangeStudentRoom
+          student={selectedStudent}
+          onClose={handleModalClose}
+        />
+      )}
+      {selectedModal === "UpdateCheckIn" && (
+        <UpdateCheckIn student={selectedStudent} onClose={handleModalClose} />
+      )}
     </div>
-          
-
-          {
-            isMoadalOpen &&(
-              <div className="modal">
-                <div className="modal-content">
-                  <h2>Select an Option</h2>
-                  <button
-                  onClick={() => handleModalSelect("UpdateStudentProfile")}
-                  className="one"
-                  >
-                    Update Student Profile
-                  </button>
-                  <button onClick={() => handleModalSelect("ChangeStudentRoom")}
-                  className="two"
-                  >
-                    Change Student Room
-                  </button>
-                  <button 
-                  onClick={() => handleModalSelect("UpdateCheckIn")}
-                  className="three"
-                  >
-
-                  </button>
-                </div>
-                </div>
-            )
-          }
-
-          {
-            selectedModal === "changeStudentProfile" && (
-              <UpdateStudentProfile 
-              student={selectedStudent}
-              onClose ={handleModalClose}
-
-             />
-            )
-          }
-          {
-            selectedModal === "changeStudentRoom" && (
-              <ChangeStudentRoom 
-              student={selectedStudent}
-              onClose ={handleModalClose}
-
-             />
-            )
-          }
-          {
-            selectedModal === "UpdateCheckIn" && (
-              <UpdateCheckIn 
-              student={selectedStudent}
-              onClose ={handleModalClose}
-
-             />
-            )
-          }
-    </div>
-   
-
-  ) 
+  );
 };
 
 export default StudentDashboard;
